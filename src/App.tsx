b293dfa56c538e287eaaa9503ef96eb2b0564150
qixiaoling <qixiaolinglinda@hotmail.com>
2022-01-06 15:09:34 +0100

    fetchdata in two ways : fetch vs axios, both cannot show updated state in the fetchData function.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {useState} from 'react';
+import {shuffleArray} from "./Utility/Util";
+const TOTAL_QUESTIONS = 10;
+export type Question = {
+    category: string;
+    correct_answer:string;
+    difficulty: string;
+    incorrect_answers : string[];
+    question: string;
+    type:string;
 }
+export type QuestionState = Question & {answers : string[]};
+export enum Difficulty {
+    EASY = "easy",
+    MEDIUM = "medium",
+    HARD = "hard",
+}
+const App: React.FC = () => {
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const startTrivia = async () =>{
+      const data = await fetchData();
+      setQuestions(data);
+  }
+
+  const fetchData = async ():Promise<QuestionState[]>  => {
+
+      const endpoint = `https://opentdb.com/api.php?amount=${TOTAL_QUESTIONS}&difficulty=${Difficulty.EASY}&type=multiple`
+      const fetchedData = fetch(endpoint)
+          .then((res) => res.json())
+          .then((res) => {
+              return res.results.map((resItem: Question) => ({
+                      ...resItem,
+                      answers: shuffleArray([...resItem.incorrect_answers, resItem.correct_answer])
+                  })// console.log(questions);//You cannot see the updated state!!!you can see it if you call the async funciton
+                  // again, but what it will show is the old data.
+              )
+
+              //below is the axios way, both fetch and axios cannot see updated state in this api call function!!!
+              // const receivedData = await axios.get(endpoint);
+              // console.log(receivedData.data.results);
+              // setQuestions(receivedData.data.results);
+              // console.log(questions);//You cannot see the updated state!!!
+
+
+          })
+      return fetchedData;
+  }
+
+  return (
+      <>
+        <div>
+          <h1>REACT QUIZ</h1>
+         <button onClick={startTrivia}>start</button>
+          {questions.map((question, index)=>{
+              const {category} = question
+            return(
+                <p key={index}>{category}</p>
+            )
+          })}
+        </div>
+      </>
+  );
+};
 
 export default App;
